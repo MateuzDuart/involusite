050958b10915ef2687940ef4b03231845ac234ae
MateuzDuart <ltbmateus@gmail.com>
2023-01-28 14:05:10 -0300

    projetos papiro

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,9 @@
-var popUpAtivado = False
+var popUpAtivado = false
 document.querySelector('li .menu').onclick = function () {
     this.classList.toggle('ativado')
     document.querySelectorAll('header li').forEach((e) => e.classList.toggle('ativado'))
 }
+
 
 function carregarProjetos() {
     const container = document.querySelector('#container')
@@ -20,14 +21,14 @@ function carregarProjetos() {
             } else {
             var verMais = `<div class="verMais"><a href="${projeto.site}" target="_blank">Ver Site</a><img class="papel-baixo" src="Imagens/parte-baixo.png" alt="parte de baixo de um papiro" srcset=""></div>`
             }
-            var projetoHTML = ` <div class="projeto" onclick="ativador(this)">
+            var projetoHTML = `<div class="containerProjeto"><div class="projeto" onclick="ativador(this)">
                 <img class="selo" src="imagens/seloPapel.png" alt="Selo de carta real" onclick="desativar(this)">
                 <img class="papel" src="Imagens/papel.svg" alt="Textura papel chique">
                 <div class="imagem"><img src="${projeto.imagen}" alt="${projeto.alt}"></div>
                 <h2>${projeto.titulo}</h2>
                 <p class="desc">${projeto.descricao.substring(0, 142)}</p>
                 ${verMais}
-                </div>`
+                </div></div>`
             
             container.innerHTML += projetoHTML
         }
@@ -40,8 +41,11 @@ function carregarProjetos() {
 function ativador(e) {
     if (!popUpAtivado) {
         var fechar = document.querySelector('.fechar')
+        var tamanho = String(window.screen.width * 5 / 1326).replace('.', '')
+        e.setAttribute('style', `transform: scale(1.${tamanho})`)
         e.classList.add('ativado')
         fechar.classList.add('ativado')
+        e.parentElement.classList.add('ativado')
         popUpAtivado = true
     }
 }
@@ -50,6 +54,7 @@ function desativar(e=false) {
     var projetos = document.querySelectorAll('.ativado')
     projetos.forEach((e) => {
         e.classList.remove('ativado')
+        e.setAttribute('style', ``)
     })
     
     setTimeout(() => {
@@ -57,3 +62,4 @@ function desativar(e=false) {
     }, 100)
     
 }
+
